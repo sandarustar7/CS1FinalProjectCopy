d4ecd25f04d7356f988597a8716f0d743e69f2d5
Spencersxtn <spencersxtn@gmail.com>
2019-04-30 07:35:51 -0600

    Initial Planning
    No functional changes here, just planning steps.

--- src/Driver.java
+++ src/Driver.java
@@ -1,10 +1,15 @@
-
+//Spencer Sexton and Sandaru Fernando
 public class Driver
 {
 
 	public static void main( String[ ] args )
 	{
-		// TODO Auto-generated method stub
+		//Step 1: Choose a race
+		//Step 2: Choose a class
+		//Step 2.5: Choose a level (add this if we have extra time)
+		//Step 3: Ability scores (potentially give the option for random rolls or points system)
+		//Step 4: Generic stuff, such as name, sex, alignment, and roll for height and weight
+		//Step 5: Equipment
 
 	}
 
